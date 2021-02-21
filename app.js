b1271b8fed56936432ca74e3efacf071eb3d7450
vishnugopy <vishnugoby0010@gmail.com>
2021-02-21 15:29:42 +0100

    :tada: With toggle buttons

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let Contentshower = document.querySelector(".Contentshower")
+let Contentshower = document.querySelector(".Contentshower");
+
 
 //content shower
-let number ;
+let number;
 let imagearr = [];
 for (let i = 1; i < 19; i++) {
     imagearr.push("Wallpapers/" + i + ".png")
@@ -9,17 +10,84 @@ for (let i = 1; i < 19; i++) {
 }
 
 imagearr.forEach(element => {
-    let figure = document.createElement("figure");
-    figure.classList.add("wallpaper")
-    figure.innerHTML =
-    `
+    let div = document.createElement("div");
+    div.classList.add("Mainbox")
+    div.innerHTML =
+        `
+    <figure class="wallpaper" id="${element.match(/\d+/)}">
     <img src="./${element}" alt="${element}">
     <figcaption>
-        <h4>${"Wallpaper_" + element.match(/\d+/)}</h4>
+        <h4>${"Wallpaper_" + element.match(/\d+/) }</h4>
         <a class="buttondownload" href="${element}" download><i class="fas fa-download"></i></a>
     </figcaption>
- 
+    </figure>
     `
-    Contentshower.appendChild(figure);
- });
+    Contentshower.appendChild(div);
+});
+
+
+let figure = document.querySelectorAll("figure");
+let light = document.querySelector(".light");
+let dark = document.querySelector(".dark");
+let all = document.querySelector(".all");
+let elementid;
+
+
+
+light.addEventListener("click", () => {
+
+    figure.forEach(element => {
+        elementid = element.id % 2;
+        console.log(elementid);
+        element.className="wallpaper";
+        if (elementid == 1) {
+            element.classList.toggle("hide");
+        }
+    });
+})
+
+
+dark.addEventListener("click", () => {
+
+    figure.forEach(element => {
+        elementid = element.id % 2;
+        element.className="wallpaper";
+        if (elementid == 0) {
+            element.classList.toggle("hide");
+            dark.classList.add("activebuttonsselecter")
+            light.classList.remove("activebuttonsselecter")
+            all.classList.remove("activebuttonsselecter")
+        }
+    });
+})
+
+light.addEventListener("click", () => {
+
+    figure.forEach(element => {
+        elementid = element.id % 2;
+        element.className="wallpaper";
+        if (elementid == 1) {
+            element.classList.toggle("hide");
+            light.classList.add("activebuttonsselecter")
+            dark.classList.remove("activebuttonsselecter")
+            all.classList.remove("activebuttonsselecter")
+            
+        }
+    });
+})
+
+
+all.addEventListener("click", () => {
+
+    figure.forEach(element => {
+       element.classList.remove("hide");
+       all.classList.add("activebuttonsselecter")
+       dark.classList.remove("activebuttonsselecter")
+       light.classList.remove("activebuttonsselecter")
+    });
+})
+
+
+
+
 
