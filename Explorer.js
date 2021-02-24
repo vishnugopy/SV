c735544c6b8199145f08d5e7c35b850b7b0ec161
vishnugopy <vishnugoby0010@gmail.com>
2021-02-24 16:47:57 +0100

    :tada: New Page

---
+++ Explorer.js
@@ -0,0 +1,23 @@
+let contents = [
+
+    {
+        "id": "1",
+        "type": "Font",
+        "name" : "lii",
+        "img" : "wallpaper2",
+        "file": "wallpaer",
+        "description": "loremmfdokhfkjdkjf",
+        "author": "HTRHGF"
+    },
+    {
+        "id": "1",
+        "type": "Font",
+        "name" : "lii",
+        "img" : "wallpaper2",
+        "file": "wallpaer",
+        "description": "loremmfdokhfkjdkjf",
+        "author": "HTRHGF"
+    },
+
+
+]
